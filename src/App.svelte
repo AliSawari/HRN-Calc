686895ec044d4c687ad06beb4c67f3cd714d1925
Ali Sawari <ali.dragon.swr@gmail.com>
2024-01-12 23:40:02 +0300

    chore: adding styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import "./main.css";
   import { onMount } from "svelte";
 
   onMount(() => {
@@ -13,6 +14,7 @@
     const nextFee =  document.getElementById('next-fee');
     const nextRealFee =  document.getElementById('next-real-fee');
     const withdrawal =  document.getElementById('withdrawal');
+    const reset =  document.getElementById('reset');
     const FEE = 0.07;
     // Magical constant that if multiplied by a numebr, it will result in a number 
     // that equals the equivalent of 7% of that number if reduced from it
@@ -36,93 +38,121 @@
         let withdraw = Math.abs(Number(finalBalance.value) - newFinal);
         nextInit.value = newFinal;
         nextFee.value = (newFinal * FEE).toFixed(1);
-        nextRealFee.value = Number(nextFee.value) - Number(debt.value).toFixed(1);
+        nextRealFee.value = (Number(nextFee.value) - Number(debt.value)).toFixed(1);
         withdrawal.value = Number(withdraw).toFixed(1);
       } else {
         nextInit.value = finalBalance.value;
         nextFee.value = (Number(finalBalance.value) * FEE).toFixed(1);
-        nextRealFee.value = Number(nextFee.value) - Number(debt.value).toFixed(1);
+        nextRealFee.value = (Number(nextFee.value) - Number(debt.value)).toFixed(1);
         withdrawal.value = 0;
       }
     }
 
+
     initBalance.onkeyup = calculate;
     finalBalance.onkeyup = calculate;
     takeFromAcc.onchange = calculate;
+    reset.onclick = () => {
+      initBalance.value = 0;
+      finalBalance.value = 0;
+      calculate();
+    }
 
   })
 </script>
 
 
+
 <main>
-  <h1>HRN Calculator</h1>
-  <div>
-    <hr>
-    <label for="init">Initial Balance account</label>
-    <input value="0" type="number" id="init"> $
+  <div class="head">
+    <h1>HRN Calculator</h1>
+    <div class="desc">A Tool to Calculate Profit, Fees, and Debts for <a href="https://www.copytradingwithhrn.com/">HRN Copy Trading Service</a></div>
   </div>
-  <div>
-    <hr>
-    <label for="fee">Starting 7% fee</label>
-    <input value="0" type="text" id="fee" readonly> $
+
+  <div class="container">
+
+    <div class="inputData">
+      <div class="item">
+        <label for="init">Initial Balance account</label>
+        
+        <input value="0" type="number" id="init" > $
+      </div>
+      <div class="item">
+        <label for="final">Final Balance</label>
+        
+        <input value="0" type="number" id="final"> $
+      </div>
+      <div class="item">
+        <label for="take-from-acc">Take Next Month's Fee from Account?</label>
+        <input type="checkbox" id="take-from-acc"> 
+      </div>
+      <div class="item">
+        <button id="reset">
+          <span class="material-symbols-outlined">
+            autorenew
+            </span>
+            <d>Reset</d>
+        </button>
+      </div>
+    </div>
+
+    <div class="output">
+
+      <div class="item">
+        <label for="fee">Starting 7% fee</label>
+        
+        <input value="0" type="text" id="fee" readonly> $
+      </div>
+  
+      <div class="item">
+        <label for="profit-each">Profit</label>
+        
+        <input value="0" type="number" id="profit" readonly> $
+      </div>
+  
+      <div class="item">
+        <label for="profit-each">Profit for each</label>
+        
+        <input value="0" type="number" id="profit-each" readonly> $
+      </div>
+  
+      <div class="item">
+        <label for="debt">Debt</label>
+        
+        <input value="0" type="number" id="debt" readonly> $
+        <p>hint: negative number means Client should pay the debt</p>
+      </div>
+  
+  
+      <div class="item">
+        <label for="next-init">Next month's Starting Balance</label>
+        
+        <input value="0" type="number" id="next-init" readonly> $
+      </div>
+  
+      <div class="item">
+        <label for="next-fee">Next month's 7% fee </label>
+        
+        <input value="0" type="number" id="next-fee" readonly> $
+      </div>
+  
+      <div class="item">
+        <label for="next-real-fee">Next month's Net fee (realized with debt)</label>
+        
+        <input value="0" type="number" id="next-real-fee" readonly> $
+      </div>
+    
+      <div class="item">
+        <label for="withdrawal">Withdrawal</label>
+        
+        <input value="0" type="number" id="withdrawal" readonly> $
+      </div>
+  
+    </div>
+
   </div>
-  <div>
-    <hr>
-    <label for="final">Final Balance</label>
-    <input value="0" type="number" id="final"> $
-  </div>
-  <div>
-    <hr>
-    <label for="profit">Profit</label>
-    <input value="0" type="number" id="profit" readonly> $
-  </div>
-  <div>
-    <hr>
-    <label for="profit-each">Profit for each</label>
-    <input value="0" type="number" id="profit-each" readonly> $
-  </div>
-  <div>
-    <hr>
-    <label for="debt">Debt</label>
-    <input value="0" type="number" id="debt" readonly> $
-    <small>hint: negative number means Client should pay the debt</small>
-  </div>
-  <div>
-    <hr>
-    <label for="take-from-acc">Take Next Month's Fee from Account?</label>
-    <input value="0" type="checkbox" id="take-from-acc"> 
-  </div>
-  <div>
-    <hr>
-    <label for="next-init">Next month's Starting Balance</label>
-    <input value="0" type="number" id="next-init" readonly> $
-  </div>
-  <div>
-    <hr>
-    <label for="next-fee">Next month's 7% fee </label>
-    <input value="0" type="number" id="next-fee" readonly> $
-  </div>
-  <div>
-    <hr>
-    <label for="next-real-fee">Next month's Net fee (realized with debt)</label>
-    <input value="0" type="number" id="next-real-fee" readonly> $
-  </div>
-  <div>
-    <hr>
-    <label for="withdrawal">Withdrawal</label>
-    <input value="0" type="number" id="withdrawal" readonly> $
-  </div>
+
+
+
+
 </main>
-
-
-<style scoped>
-  main {
-    font-family: Verdana;
-  }
-
-  input {
-    margin: 0 10px;
-    padding: 5px 15px;
-    font-size: 20px;
-  }
-</style>
